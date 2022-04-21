d59ad7e8e0e6c997691aba3d52c5e0b09ce76f1e
GatosLoco1990 <ricardogandica@hotmail.com>
2022-04-20 20:28:36 -0500

    Avance punto 1

--- Principal.java
+++ Principal.java
@@ -6,6 +6,8 @@ public class Principal {
         Scanner teclado = new Scanner(System.in);
         int n;
         int a = 0;
+        int i = 0;
+        Zombie[] arr = new Zombie[100];
 
         while (a == 0) {
             System.out.println("Ingrese un dígito del 1 al 9");
@@ -16,6 +18,17 @@ public class Principal {
                     a = 1;
                     break;
                 case 1:
+                    System.out.println("Ingrese el nombre, salud, fecha de nacimiento y tipo de sangre del zombie: ");
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = teclado.next();
+                    System.out.println("Ingrese la salud: ");
+                    int salud = teclado.nextInt();
+                    System.out.println("Ingrese el Tipo de sangre: ");
+                    String nombre = teclado.next();
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = teclado.next();
+                    arr[i] = new 
+
                     break;
 
             }
