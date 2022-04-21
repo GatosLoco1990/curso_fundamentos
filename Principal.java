b826d792a89658f8cdfe39ae04847a073f2f925f
GatosLoco1990 <ricardogandica@hotmail.com>
2022-04-20 21:21:20 -0500

    Fin punto 1, y avance punto 2

--- Principal.java
+++ Principal.java
@@ -35,6 +35,24 @@ public class Principal {
                     String sang = teclado.next();
 
                     arr[i] = new Zombie(nombre, salud, fenac, sang);
+                    i++;
+                    System.out.print("\033[H\033[2J");
+                    System.out.flush();
+
+                    break;
+                case 2:
+                    for (int j = 0; i - 1 <= i; j++) {
+                        System.out.println("El nombre del zombie [" + j + "] es: " + arr[j].getNombre());
+                        System.out.println("La salud del zombie [" + j + "] es: " + arr[j].getSalud());
+                        System.out
+                                .println("La fecha de nacimiento del zombie [" + j + "] es: " + arr[j].getFecha_nac());
+                        System.out.println("El tipo de sangre del zombie [" + j + "] es: " + arr[j].getTipo_sang());
+                        System.out.println("");
+                    }
+                    // System.out.println("Presione 'F' para continuar");
+
+                    // System.out.print("\033[H\033[2J");
+                    // System.out.flush();
 
                     break;
 
