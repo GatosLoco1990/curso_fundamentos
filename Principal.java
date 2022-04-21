c269de157b5214dc11b6cad7d0890cbec935e8fe
GatosLoco1990 <ricardogandica@hotmail.com>
2022-04-20 20:43:22 -0500

    Publicación rama Samuel

--- Principal.java
+++ Principal.java
@@ -3,10 +3,18 @@ import java.util.Scanner;
 
 public class Principal {
     public static void main(String[] args) throws Exception {
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu() {
+
         Scanner teclado = new Scanner(System.in);
+
         int n;
         int a = 0;
+        int i = 0;
 
+        Zombie[] arr = new Zombie[100];
         while (a == 0) {
             System.out.println("Ingrese un dígito del 1 al 9");
             n = teclado.nextInt();
@@ -16,9 +24,23 @@ public class Principal {
                     a = 1;
                     break;
                 case 1:
+                    System.out.println("Ingrese el nombre, salud, fecha de nacimiento y tipo de sangre del zombie: ");
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = teclado.next();
+                    System.out.println("Ingrese la salud: ");
+                    int salud = teclado.nextInt();
+                    System.out.println("Ingrese fecha de nacimiento: ");
+                    String fenac = teclado.next();
+                    System.out.println("Ingrese el tipo de sangre: ");
+                    String sang = teclado.next();
+
+                    arr[i] = new Zombie(nombre, salud, fenac, sang);
+
                     break;
 
             }
         }
+        teclado.close();
+
     }
 }
